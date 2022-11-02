a5dae005f6eda7a225cc8e9090251e53266e1adc
Priyanka9120 <priyanka912066@gmail.com>
2022-11-02 15:13:16 +0530

    middleware 1 assignment donr

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const moment = require('moment')
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,15 +12,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
-app.use (
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+// ASSIGNMENT :-
+// Write a middleware that logs (console.log) some data everytime any API is hit
+// Data to be logged:-the current timestamp(as date time) , the IP of the user and the route being requested).
+// For this first figure out how to get the route location being request, how to get current timestamp and how to get the IP.
+// NOTE: ip of local computer will come as ::1 so dont get disturbed by seeing this)
+//  
+// e.g: you should be logging something like this on each line:
+// time , IP, Route should be printed on each line in terminal( every time an api is hit)
+// 2010-08-19 14:00:00 , 123.459.898.734 , /createUser
+app.use(
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+        let ip = req.ip   //req.ip property contains the remote IP address of the request 
+        let url = req.url
+        // let ts=Date()   //timestamp
+        let ts = moment().format('YYYY-MM-DD HH:mm:ss')
+        console.log(ts + ', ' + ip + ', ' + url)
+        console.log(`${ts}, ${ip}, ${url}`)
+
+        res.send({ ts: ts, ip: ip, router: url })
+    })
+
 
 app.use('/', route);
 
