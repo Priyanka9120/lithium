0c4481582ada73995059e634415757486372491b
Priyanka9120 <priyanka912066@gmail.com>
2022-11-06 23:48:13 +0530

    assignment done auth-02

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://priyanka912066:Pie5MEDyx8B1zOiq@cluster0.ucnslwp.mongodb.net/Priyanka-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
